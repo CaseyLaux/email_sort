f6a7c5da9d0ab63c89ff7e98f687a4abe2de7e2b
casey <caseylaux631@gmail.com>
2023-05-21 21:12:47 -0400

    Pull and use databse fully

--- front-end/email-review/sorting_server/app.py
+++ front-end/email-review/sorting_server/app.py
@@ -1,71 +1,77 @@
+from bson import json_util, ObjectId
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
-import shutil
 import configparser
 from pymongo import MongoClient
-import email_indexer
 
 # Define constants for paths
-BASE_PATH = os.path.join('..', 'public')
 
 # Read settings from a configuration file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 # Get MongoDB settings from the config file
-mongo_settings = {key: config.get('mongo', key) for key in ['uri', 'database', 'body_collection', 'bodyless_collection']}
+mongo_settings = {key: config.get('mongo', key) for key in ['uri', 'database', 'body_collection', 'bodyless_collection', 'unsorted_collection']}
 
 # Get MongoDB connection and collections
 client = MongoClient(mongo_settings['uri'])
 db = client[mongo_settings['database']]
 body_collection_db = db[mongo_settings['body_collection']]
 bodyless_collection_db = db[mongo_settings['bodyless_collection']]
-
+unsorted_collection_db = db[mongo_settings['unsorted_collection']]
 app = Flask(__name__)
 CORS(app)  # Enable CORS for the Flask app
 
-def handle_email_move(original_path, new_path, email_data):
+#Gather emails from the database
+@app.route('/api/get-emails', methods=['GET'])
+def get_emails():
+    body_emails = list(body_collection_db.find())
+    unsorted_emails = list(unsorted_collection_db.find())
+    return json_util.dumps({'body_emails': body_emails, 'unsorted_emails': unsorted_emails}), 200
+def handle_email_move(email_data):
     # Move the email
     try:
-        shutil.move(original_path, new_path)
         # Insert data to MongoDB
         body_collection_db.insert_one(email_data['email'])
         bodyless_collection_db.insert_one(email_data['bodyless_email'])
     except Exception as e:
         print(f"Error moving or saving email: {e}")
         return jsonify({'error': f'Failed to move or save email: {str(e)}'}), 500
-
 @app.route('/api/move-email', methods=['POST'])
 def move_email():
     data = request.get_json()
-    original_path = os.path.join(BASE_PATH, data['originalPath'])
-    new_path = os.path.join(BASE_PATH, data['newPath'])
+    email_id = ObjectId(data['email']['_id'])
 
-    if not os.path.exists(original_path):
-        return jsonify({'error': 'Source file not found.'}), 400
+    # Delete the email from the unsorted collection
+    
 
-    response = handle_email_move(original_path, new_path, data)
-    email_indexer.index_emails()
-    return response if response else jsonify({'message': 'Email moved successfully.'}), 200
+    # Insert the updated email into the body collection
+    try:
+        body_collection_db.insert_one(data['email'])
+    except Exception as e:
+        print(f"Error inserting email: {e}")
+        return jsonify({'error': f'Failed to insert email: {str(e)}'}), 500
+    try:
+        unsorted_collection_db.delete_one({'_id': email_id})
+    except Exception as e:
+        print(f"Error deleting email: {e}")
+        return jsonify({'error': f'Failed to delete email: {str(e)}'}), 500
+
+    return jsonify({'message': 'Email moved successfully.'}), 200
 
 @app.route('/api/delete-email', methods=['POST'])
 def delete_email():
     data = request.get_json()
-    original_path = os.path.join(BASE_PATH, data['originalPath'])
+    email_id = ObjectId(data['email']['_id'])
 
-    if not os.path.exists(original_path):
-        return jsonify({'error': 'Source file not found.'}), 400
-
-    # Delete the email
+    # Delete the email from the database
     try:
-        os.remove(original_path)
+        unsorted_collection_db.delete_one({'_id': email_id})
     except Exception as e:
-        print(f"Error deleting file: {e}")
+        print(f"Error deleting email: {e}")
         return jsonify({'error': f'Failed to delete email: {str(e)}'}), 500
 
-    email_indexer.index_emails()
     return jsonify({'message': 'Email deleted successfully.'}), 200
-
 if __name__ == '__main__':
     app.run(port=config.getint('app', 'port'))
--- front-end/email-review/src/EmailViewer.js
+++ front-end/email-review/src/EmailViewer.js
@@ -12,12 +12,16 @@ const EmailViewer = () => {
   const [classification, setClassification] = useState('');
   const [unsortedFilenames, setUnsortedFilenames] = useState([]);
   
-  const deleteEmail = async (email, originalPath) => {
+  const deleteEmail = async (email) => {
+    const email_id = {
+      ...email,
+      _id: email._id.$oid,
+    };
     try {
       const response = await fetch('http://localhost:3001/api/delete-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ originalPath }),
+        body: JSON.stringify({ email: email_id }),
       });
   
       if (!response.ok) {
@@ -25,10 +29,9 @@ const EmailViewer = () => {
       }
   
       // Update unsortedEmails state to remove the deleted email
-      setUnsortedEmails((prevState) => prevState.filter((e) => e !== email));
+      setUnsortedEmails((prevState) => prevState.filter((e) => e._id !== email._id));
   
       alert('Email deleted successfully.');
-      
     } catch (error) {
       console.error(error);
       alert('Failed to delete email.');
@@ -36,66 +39,24 @@ const EmailViewer = () => {
   };
 
   useEffect(() => {
-    async function loadHumanSortedEmails() {
+    async function loadEmails() {
       try {
-        const humanSortedUrl = `${process.env.PUBLIC_URL}/human_sorted/index.json`;
-        console.log('Fetching human_sorted index:', humanSortedUrl);
-        const humanSortedResponse = await fetch(humanSortedUrl);
-        const humanSortedFilenames = await humanSortedResponse.json();
-
-        const humanSortedEmailPromises = humanSortedFilenames.map(async (filename) => {
-          try {
-            const emailResponse = await fetch(`${process.env.PUBLIC_URL}/human_sorted/${filename}`);
-            if (!emailResponse.ok) {
-              console.error(`Error fetching file: ${filename}`);
-              return;
-            }
-            return await emailResponse.json();
-          } catch (error) {
-            console.error(`Error parsing JSON from file: ${filename}`, error);
-          }
-        });
-        
-
-        const loadedHumanSortedEmails = await Promise.all(humanSortedEmailPromises);
-        return loadedHumanSortedEmails;
+        const response = await fetch('http://localhost:3001/api/get-emails');
+        const data = await response.json();
+        console.log(data);
+        setHumanSortedEmails(data.body_emails);
+        setUnsortedEmails(data.unsorted_emails);
+        console.log(humanSortedEmails);
+        console.log(unsortedEmails);
       } catch (error) {
-        console.error('Error loading human_sorted emails:', error);
+        console.error('Error loading emails:', error);
       }
     }
 
-    async function loadUnsortedEmails() {
-      try {
-        const unsortedUrl = `${process.env.PUBLIC_URL}/unsorted/index.json`;
-        console.log('Fetching unsorted index:', unsortedUrl);
-        const unsortedResponse = await fetch(unsortedUrl);
-        const unsortedFilenames = await unsortedResponse.json();
-
-        const start = 0;
-        const limit = 50;
-        const end = Math.min(start + limit, unsortedFilenames.length);
-
-        const unsortedEmailPromises = unsortedFilenames.slice(start, end).map(async (filename) => {
-          console.log(`loading  ${filename}`)
-          const emailResponse = await fetch(`${process.env.PUBLIC_URL}/unsorted/${filename}`);
-          return await emailResponse.json();
-        });
-
-        const loadedUnsortedEmails = await Promise.all(unsortedEmailPromises);
-        return { unsorted: loadedUnsortedEmails, filenames: unsortedFilenames };
-      } catch (error) {
-        console.error('Error loading unsorted emails:', error);
-      }
-    }
-
-    loadHumanSortedEmails().then(setHumanSortedEmails);
-    loadUnsortedEmails().then((loadedUnsortedEmails) => {
-      setUnsortedEmails(loadedUnsortedEmails.unsorted);
-      setUnsortedFilenames(loadedUnsortedEmails.filenames);
-    });
+    loadEmails();
   }, []);
 
-  const moveEmail = async (email, originalPath) => {
+  const moveEmail = async (email) => {
     // Validate rating and classification inputs
     if (!rating || !classification) {
       alert('Please enter a rating and classification.');
@@ -104,24 +65,16 @@ const EmailViewer = () => {
   
     // Add classification and rating to the email completion field
     const updatedEmail = {
-      prompt: `Subject: ${email.email_subject}\nFrom:${email.email_sender}\nDate:${email.email_date}\nContent:${email.prompt.split('\n\n###\n\n')[1]}\n\n###\n\n`,
+      ...email,
       completion: `Classification: ${classification}, Rating: ${rating}`,
-    };
-    const updatedEmail_bodyless = {
-      prompt: `Subject: ${email.email_subject}\nFrom:${email.email_sender}\nDate:${email.email_date}\nContent:${email.prompt}\n\n###\n\n`,
-      completion: `Classification: ${classification}, Rating: ${rating}`,
+      _id: email._id.$oid,
     };
   
-    const newPath = `human_sorted\\${classification}_${rating}_${email.email_date.replace(/[^a-zA-Z0-9]/g, '_')}_${email.email_sender.replace(/[^a-zA-Z0-9]/g, '_')}.json`;
-    const new_bodyless_Path = `human_sorted\\${classification}_${rating}_${email.email_date.replace(/[^a-zA-Z0-9]/g, '_')}_${email.email_sender.replace(/[^a-zA-Z0-9]/g, '_')} _bodyless.json`;
-
-    // Log the filename before saving it
-    console.log('Saving email to:', newPath);
     try {
       const response = await fetch('http://localhost:3001/api/move-email', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: updatedEmail, originalPath, newPath, bodyless_email: updatedEmail_bodyless, new_bodyless_Path}),
+        body: JSON.stringify({ email: updatedEmail }),
       });
   
       if (!response.ok) {
@@ -129,15 +82,13 @@ const EmailViewer = () => {
       }
   
       // Update unsortedEmails state to remove the moved email
-      setUnsortedEmails((prevState) => prevState.filter((e) => e !== email));
+      setUnsortedEmails((prevState) => prevState.filter((e) => e._id !== email._id));
   
       // Reset rating and classification inputs
       setRating('');
       setClassification('');
   
       alert('Email moved successfully.');
-
-      
     } catch (error) {
       console.error(error);
       alert('Failed to move email.');
@@ -156,7 +107,7 @@ const EmailViewer = () => {
     <div className="email-viewer-container">
       <div className="email-box human-sorted">
         <h2>Human Sorted Emails</h2>
-        <Email email={humanSortedEmails[humanSortedIndex]} />
+        <Email email={humanSortedEmails[humanSortedIndex] ? humanSortedEmails[humanSortedIndex] : null} />
         <EmailControls
           changeIndex={changeIndex}
           currentIndex={humanSortedIndex}
@@ -170,7 +121,7 @@ const EmailViewer = () => {
           Unsorted Emails{' '}
           {unsortedEmails[unsortedIndex] && unsortedFilenames[unsortedIndex]}
         </h2>
-        <Email email={unsortedEmails[unsortedIndex]} />
+        <Email email={unsortedEmails[unsortedIndex] ? unsortedEmails[unsortedIndex] : null} />
         <div>
           <label htmlFor="rating">Rating (1-10): </label>
           <input
