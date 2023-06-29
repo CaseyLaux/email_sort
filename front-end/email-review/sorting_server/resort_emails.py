264b28f6a05bf9cb9d95fc4839cb59df2a727044
casey <caseylaux631@gmail.com>
2023-06-28 23:31:58 -0400

    fixing branches

--- front-end/email-review/sorting_server/resort_emails.py
+++ front-end/email-review/sorting_server/resort_emails.py
@@ -5,50 +5,53 @@ import subprocess
 import os
 import test_autoformat
 import openai
-sys.path.append("C:\\Users\\casey\\PycharmProjects\\email_sort\\")
+sys.path.append("C:\\Users\\casey\\PycharmProjects\\email_sort")
 
-from secrats import ColinGTK
 
-client = MongoClient('localhost', 27017)
-db = client[ColinGTK().Account_ID]
 
-collection_string = ColinGTK().user + "re-sorted"
-collection = db[collection_string]
 
-def delete_documents(ids_to_delete):
-    for id in ids_to_delete:
-        print(id)
-        collection.delete_one({"_id": id})
-def database_pull():
-    # Fetch all documents from the collection, but only include the 'email_prompt' and 'completion' fields
-    documents = collection.find({}, {"prompt": 1, "completion": 1, "_id": 1})
 
-    # Convert MongoDB Cursor to list of dicts
-    documents_list = list(documents)
-    ids_to_delete = [document['_id'] for document in documents_list]
-    if documents_list == []:
-        exit(print("No documents found in collection"))
-    
-    # Remove the '_id' key from each document
-    for document in documents_list:
-        document.pop('_id', None)
-    
-    base_filename = "re-train_gpt_data_prime"
-    extension = ".json"
-    counter = 0
-    filename = base_filename + extension
+def resort_emails(username):
+    client = MongoClient('localhost', 27017)
+    db = client[username]
+    def delete_documents(ids_to_delete):
+        for id in ids_to_delete:
+            print(id)
+            collection.delete_one({"_id": id})
 
-    # If filename exists, iterate the counter and append it to the filename
-    while os.path.exists(filename):
-        counter += 1
-        filename = "{}_{}{}".format(base_filename, counter, extension)
+    collection_string = "re-sorted"
+    collection = db[collection_string]
 
-    # Write the documents to a json file
-    with open(filename, 'w') as file:
-        file.write(json.dumps(documents_list, indent=4))
-    return filename, ids_to_delete
+    def database_pull():
 
-def resort_emails():
+        # Fetch all documents from the collection, but only include the 'email_prompt' and 'completion' fields
+        documents = collection.find({}, {"prompt": 1, "completion": 1, "_id": 1})
+
+        # Convert MongoDB Cursor to list of dicts
+        documents_list = list(documents)
+        ids_to_delete = [document['_id'] for document in documents_list]
+        if documents_list == []:
+            exit(print("No documents found in collection"))
+        
+        # Remove the '_id' key from each document
+        for document in documents_list:
+            document.pop('_id', None)
+        
+        base_filename = "re-train_gpt_data_prime"
+        extension = ".json"
+        counter = 0
+        filename = base_filename + extension
+
+        # If filename exists, iterate the counter and append it to the filename
+        while os.path.exists(filename):
+            counter += 1
+            filename = "{}_{}{}".format(base_filename, counter, extension)
+
+        # Write the documents to a json file
+        with open(filename, 'w') as file:
+            file.write(json.dumps(documents_list, indent=4))
+        return filename, ids_to_delete
+
     filename, deleteData = database_pull()
     test_autoformat.autoformat(filename)
     openai.api_key = "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"
